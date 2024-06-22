538e3cad2b493323d181450e63708b36a550f725
Abhishek Aralikatti <abhishekaralikatti1009@gmail.com>
2024-06-22 12:19:06 +0200

    changed the dailogue

--- C++/Projects/my_first.cpp
+++ C++/Projects/my_first.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello, World! I am a C++ superhero!" << std::endl;
+    std::cout << "Hello, World! I am a C++ superhero! this is my first project" << std::endl;
     return 0;
 }
